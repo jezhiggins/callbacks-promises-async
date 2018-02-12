f1f8ff9605647de2631efba90b9c02b1fa6fcfa8
Jez Higgins <jez@jezuk.co.uk>
2018-02-12 21:44:54 +0000

    Rework tests to return the promises

---
+++ promises-3/test/test.js
@@ -0,0 +1,52 @@
+/* eslint-env mocha */
+
+const chai = require('chai')
+const dirtyChai = require('dirty-chai')
+const expect = chai.expect
+chai.use(dirtyChai)
+const assert = require('assert')
+
+const readdirtree = require('../index.js')
+
+const fixtures = `${__dirname}/../../test-fixture`
+
+describe('directory reading tests', () => {
+  it('one level deep', () => {
+    return readdirtree(`${fixtures}/one-level`)
+      .then(files => {
+        expect(files.length).to.equal(2)
+        expect(files).to.eql([
+          'file-1',
+          'file-2'
+        ])
+      })
+  })
+
+  it('multiple levels deep', () => {
+    return readdirtree(`${fixtures}/multi-level`)
+      .then(files => {
+        expect(files.length).to.equal(13)
+        expect(files).to.eql([
+          'a',
+          'b/a',
+          'b/b',
+          'c',
+          'd/a/a',
+          'd/a/b',
+          'd/b',
+          'd/c',
+          'd/d/a',
+          'd/d/b/a',
+          'd/d/b/b',
+          'e',
+          'f'
+        ])
+      })
+  })
+
+  it('non-existent directory errors out', () => {
+    return readdirtree(`${fixtures}/does-not-exist`)
+      .then(() => assert.fail())
+      .catch(() => { })
+  })
+})
