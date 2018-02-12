9873799e7483d8938bc45c82a8d72b78c17e0eb4
Jez Higgins <jez@jezuk.co.uk>
2018-02-12 15:31:30 +0000

    Node style callbacks

---
+++ index.js
@@ -0,0 +1,47 @@
+const fs = require('fs')
+
+function readdirtree (root, callback) {
+  walktree(root, '', callback)
+}
+
+function walktree (root, prefix, callback) {
+  fs.readdir(root, (err, paths) => {
+    if (err) {
+      return callback(err)
+    }
+
+    checkPaths(root, paths, 0, prefix, [], callback)
+  })
+} // walktree
+
+function checkPaths (rootPath, paths, index, prefix, found, callback) {
+  if (index === paths.length) {
+    return callback(null, found)
+  }
+
+  const path = paths[index]
+  const fullPath = `${rootPath}/${path}`
+  const localPath = `${prefix}${path}`
+  const next = () => checkPaths(rootPath, paths, index + 1, prefix, found, callback)
+
+  fs.stat(fullPath, (err, stats) => {
+    if (err) {
+      callback(err)
+    } else if (stats.isFile()) {
+      found.push(localPath)
+      next()
+    } else if (stats.isDirectory()) {
+      walktree(fullPath, `${localPath}/`, (err, files) => {
+        if (err) {
+          callback(err)
+        }
+        found.push(...files)
+        next()
+      })
+    } else {
+      next()
+    }
+  })
+} // checkPaths
+
+module.exports = readdirtree
