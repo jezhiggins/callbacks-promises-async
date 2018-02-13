1d34b4a55cb0a194a9737654d2a657ab678bae9c
Jez Higgins <jez@jezuk.co.uk>
2018-02-13 12:57:30 +0000

    Switch walktree to async

---
+++ async-await-2/test/test.js
@@ -0,0 +1,53 @@
+/* eslint-env mocha */
+
+const chai = require('chai')
+const dirtyChai = require('dirty-chai')
+const expect = chai.expect
+chai.use(dirtyChai)
+const assert = require('assert')
+
+const readdirtree = require('../index.js')
+
+const fixtures = `${__dirname}/../../test-fixture`
+
+describe('async/await implementation', () => {
+  it('one level deep', async () => {
+    const files = await readdirtree(`${fixtures}/one-level`)
+
+    expect(files.length).to.equal(2)
+    expect(files).to.eql([
+      'file-1',
+      'file-2'
+    ])
+  })
+
+  it('multiple levels deep', async () => {
+    const files = await readdirtree(`${fixtures}/multi-level`)
+
+    expect(files.length).to.equal(13)
+    expect(files).to.eql([
+      'a',
+      'b/a',
+      'b/b',
+      'c',
+      'd/a/a',
+      'd/a/b',
+      'd/b',
+      'd/c',
+      'd/d/a',
+      'd/d/b/a',
+      'd/d/b/b',
+      'e',
+      'f'
+    ])
+  })
+
+  it('non-existent directory errors out', async () => {
+    try {
+      await readdirtree(`${fixtures}/does-not-exist`)
+    } catch (err) {
+      return // that's what we want
+    }
+    assert.fail()
+  })
+})
